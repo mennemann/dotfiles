88399326e0adc9d2ffe33357c639baa77b060cd6
Marcin Mennemann <marcin@mennemann.net>
2025-02-04 11:09:43 +0100

    nvim latex viewer changes

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -1,7 +1,11 @@
 return {
     "lervag/vimtex",
     init = function()
-        vim.g.vimtex_view_method = "zathura"
+        if vim.fn.has('win32') == 1 then
+            vim.g.vimtex_view_method = "general"
+        elseif vim.fn.has('unix') == 1 then
+            vim.g.vimtex_view_method = "zathura"
+        end
 
         vim.g.vimtex_view_forward_search_on_start = false
 
