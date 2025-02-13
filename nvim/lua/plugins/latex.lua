89e002d0dac72190d74f375cab4795a93a6652e3
Marcin Mennemann <marcin@mennemann.net>
2025-02-13 10:33:59 +0100

    nvim adjusted latex stuff

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -7,15 +7,12 @@ return {
             vim.g.vimtex_view_method = "zathura"
         end
 
+        vim.g.vimtex_mappings_prefix = "<leader>l"
+
         vim.g.vimtex_view_forward_search_on_start = false
 
         vim.g.vimtex_compiler_latexmk = {
             out_dir = "out",
-            options = {
-                "-pdf",
-                "-interaction=nonstopmode",
-                "-synctex=1",
-            },
         }
     end,
 }
